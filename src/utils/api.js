06a76e5af0ff0a0d8e73a6d55e27b0ed391bf798
MValenciaolartre <124168851+MValenciaolartre@users.noreply.github.com>
2025-11-02 14:40:35 -0300

    ...
    ..

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://tuabogadoenlinea.free.nf/apis';
+const API_URL = import.meta.env.VITE_API_URL || 'http://tuabogadoenlinea.free.nf/apis/api/';
 
 // Crear instancia de axios
 const api = axios.create({
